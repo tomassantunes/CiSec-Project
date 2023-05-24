998f215a1d74b7017de0a8aa7a40231dc805d507
tomassantunes <tomasbantunes@gmail.com>
2023-05-24 23:16:52 +0100

    para apagar lol, vou fazer em python :)

--- poli-virus/main.s
+++ poli-virus/main.s
@@ -5,22 +5,15 @@ section .text
         mov rcx, counter
         
     loop_start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, base_filename
-        mov rdx, extension
-        mov r10, rcx
-        call construct_filename
-
         mov rax, 2
-        lea rdi, [rel filename]
+        lea rdi, base_filename
         mov rsi, 0x0211
         mov rdx, 0644
         syscall
 
         cmp rax, 0
         jl error
-
+        
         mov rdi, rax
         mov rax, 1
         mov rsi, buffer
@@ -38,29 +31,6 @@ section .text
         mov rdi, 42
         syscall
 
-    construct_filename:
-        push rbx
-        push r11
-
-        mov rbx, 10
-        xor r11, r11
-
-        filename_loop:
-            xor rdx, rdx
-            div rbx
-            add dl, '0'
-            dec rsi
-            mov byte [rsi], dl
-            inc r11
-            test rax, rax
-            jnz filename_loop
-
-        mov byte [rsi], '+'
-
-        pop r11
-        pop rbx
-        ret
-
     error:
         mov rax, 1
         mov rdi, 1
@@ -74,9 +44,8 @@ section .text
         
 
 section .data
-    base_filename: db "quine", 0
-    filenameLen: equ $-base_filename
-    extension: db ".txt", 0
+    base_filename: db "quine.txt", 0
+    base_filenameLen: equ $-base_filename
 
     buffer: db "this is a test", 0
     bufferLen: equ $-buffer
@@ -84,7 +53,4 @@ section .data
     errorm: db "An error occurred.", 0
     errormLen: equ $-errorm
 
-    counter: equ 5
-
-section .bss
-    filename resb 16
+    counter: equ 1
---
+++ poli-virus/run_encrypt_file.sh
@@ -0,0 +1,3 @@
+nasm -felf64 encrypt_file.s
+ld encrypt_file.o -o encrypt_file
+./encrypt_file
